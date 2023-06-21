2b0712419b99c5cc4cb141e0855b23e05e84c3fd
Liviu <liviutoma946@gmail.com>
2023-06-21 20:26:18 +0300

    populated database

---
+++ controllers/products.js
@@ -0,0 +1,20 @@
+const Product = require('../models/product');
+
+const getAllProductsStatic = async (req, res) => {
+    const products = await Product.find({ 
+        featured: true, 
+    });
+    res.status(200).json({ 
+        products, 
+        nbHits: products.length 
+    });
+}
+
+const getAllProducts = async (req, res) => {
+    res.status(200).json({ msg: 'products route' });
+}
+
+module.exports = {
+    getAllProducts,
+    getAllProductsStatic
+}
